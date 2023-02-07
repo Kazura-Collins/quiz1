7121d3e1be40c8759aa8370286cbfc748277af68
Kazura-Collins <98463668+Kazura-Collins@users.noreply.github.com>
2023-02-07 08:03:45 -0500

    quiz 1 js
    no sé pq no logré el + both

---
+++ prueba.js
@@ -0,0 +1,14 @@
+i=1
+for (i>0; i<50; i++){
+    console.log (i);
+    if (i%3===0) {
+        console.log(i+ " by 3")}
+    if (i%5===0) {
+        console.log(i+ " by 5")
+    }
+    if (i%3===0, i%5===0) {
+        console.log(i+ " by both")
+    }
+}
+
+
